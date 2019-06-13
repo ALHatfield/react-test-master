e39c2a0632e24f849fe4bb4481b08f338735abfe
andrew <andrew8412@gmail.com>
2019-06-13 03:02:01 -0400

    added bootstrap, pages folder and added empty components

---
+++ client/src/pages/Product.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class Product extends Component {
+    render() {
+        return (
+            <div className="container">
+                <div className="row">
+                    <div className="col-12 col-md-6">
+                        product info ...
+                    </div>
+                    <div className="col-12 col-md-6">
+                        product picture ...
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Product;
