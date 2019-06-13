ad423c5feb8b090eae371f145f1e614eafabd584
andrew <andrew8412@gmail.com>
2019-06-13 02:19:35 -0400

    created 'feature' branch and isolated product page as its own component

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
+import ProductPage from './components/ProductPage';
 
 function App() {
   return (
     <div className="App">
 
-      product page...
+      <ProductPage />
 
     </div>
   );
