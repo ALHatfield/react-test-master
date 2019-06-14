fc768eddd754e49a87d3baaad33c98d63e21956e
andrew <andrew8412@gmail.com>
2019-06-13 21:52:51 -0400

    new branch

--- client/src/App.js
+++ client/src/App.js
@@ -1,39 +1,36 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import ProductPage from './pages/Product';
 import img1 from './ath-msr7-black.jpg';
 import img2 from './ath-msr7-brown.jpg';
 
-class App extends Component {
-  state = {
-    product: {
-      name: "the thing's name",
-      awards: "all the awards",
-      images: [
-        {
-          src: img1,
-          color: "black"
-        },
-        {
-          src: img2,
-          color: "brown"
-        }
-      ],
-      description: "this thing is great",
-      details: "so many details",
-      discountPrice: 59.99,
-      retailPrice: 89.99,
-    }
-  }
-  
 
-  render() {
-    return (
-      <div className="App">
-        <ProductPage product={this.state.product} />
-      </div>
-    );
-  }
-}
+// use "use effect" for life cycle methods
+const App = () => {
+  const [product, setProduct] = useState({
+    name: "the thing's name",
+    awards: "all the awards",
+    images: [
+      {
+        src: img1,
+        color: "black"
+      },
+      {
+        src: img2,
+        color: "brown"
+      }
+    ],
+    description: "this thing is great",
+    details: "so many details",
+    discountPrice: 59.99,
+    retailPrice: 89.99,
+        })
+  return (
+    <div className="App">
+      <ProductPage product={product} setProduct={setProduct}/>
+    </div>
+  )
+};
+
 
 export default App;
--- client/src/pages/Product.js
+++ client/src/pages/Product.js
@@ -3,134 +3,151 @@ import ProductHeader from '../components/ProductHeader';
 import ProductImage from '../components/ProductImage';
 import ProductDescription from '../components/ProductDescription';
 
-class Product extends Component {
-    state = {
-        src: "",
-        color: "",
-        text: "",
-        cartText: "Add to Cart",
-        inCart: false
-    }
+// const Product =({
+//   product  
+// }) => {
+//     state = {
+//         src: "",
+//         color: "",
+//         text: "",
+//         cartText: "Add to Cart",
+//         inCart: false
+//     }
 
-    componentDidMount() {
-        this.setState({
-            src: this.props.product.images[0].src,
-            color: this.props.product.images[0].color,
-            text: "description"
-        })
-    }
+//     componentDidMount() {
+//         this.setState({
+//             src: product.images[0].src,
+//             color: product.images[0].color,
+//             text: "description"
+//         })
+//     }
 
 
-    handleTextChange = text => {
-        this.setState({
-            text: text
-        })
-    }
+//     handleTextChange = text => {
+//         this.setState({
+//             text: text
+//         })
+//     }
 
-    renderDescription = () => {
-        return(
-            <div>{this.props.product.description}</div>
-        )
-    } 
+//     renderDescription = () => {
+//         return(
+//             <div>{this.props.product.description}</div>
+//         )
+//     } 
     
-    renderDetails = () => {
-        return(
-            <div>{this.props.product.details}</div>
-        )
-    }
+//     renderDetails = () => {
+//         return(
+//             <div>{this.props.product.details}</div>
+//         )
+//     }
 
-    handleColorOptionChange = (color) => {
-        const currentItem = this.props.product.images.find(image => {
-             return image.color === this.state.color
-        });
+//     handleColorOptionChange = (color) => {
+//         const currentItem = this.props.product.images.find(image => {
+//              return image.color === this.state.color
+//         });
 
-        this.setState({
-            color: color,
-            src: currentItem.src
-        })        
-    }
+//         this.setState({
+//             color: color,
+//             src: currentItem.src
+//         })        
+//     }
 
-    addToCart = () => {
-        if(!this.state.inCart) {
-            this.setState({
-                cartText: "Loading..."
-            })
-            setTimeout(() => {
-                this.setState({
-                    cartText: "View Cart",
-                    inCart: true
-                })
-            }, 2000)
-        }
-    }
+//     addToCart = () => {
+//         if(!this.state.inCart) {
+//             this.setState({
+//                 cartText: "Loading..."
+//             })
+//             setTimeout(() => {
+//                 this.setState({
+//                     cartText: "View Cart",
+//                     inCart: true
+//                 })
+//             }, 2000)
+//         }
+//     }
     
 
 
     
-    render() {
-        const { name, awards, images, discountPrice, retailPrice, colors } = this.props.product;
-        return (    
-            <div className="container">
-                <div className="row">
+//     render() {
+//         const { name, awards, images, discountPrice, retailPrice, colors } = this.props.product;
+//         return (    
+//             <div className="container">
+//                 <div className="row">
                 
-                    <div className="col-12 col-md-6">
-                        <ProductHeader name={name} awards={awards} handleTextChange={this.handleTextChange} />
+//                     <div className="col-12 col-md-6">
+//                         <ProductHeader name={name} awards={awards} handleTextChange={this.handleTextChange} />
                         
-                        {
-                            this.state.text === 'description'
-                                ? this.renderDescription()
-                                : this.renderDetails()
-                        }
+//                         {
+//                             this.state.text === 'description'
+//                                 ? this.renderDescription()
+//                                 : this.renderDetails()
+//                         }
 
 
-                        <div>
-                            <span>{discountPrice}</span><span>{retailPrice}</span>
-                        </div>
+//                         <div>
+//                             <span>{discountPrice}</span><span>{retailPrice}</span>
+//                         </div>
 
 
-                        <div>Colors</div>
+//                         <div>Colors</div>
                         
-                        <div className="dropdown show">
-                            <a className="btn btn-secondary dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                {this.state.color}
-                            </a>
+//                         <div className="dropdown show">
+//                             <a className="btn btn-secondary dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+//                                 {this.state.color}
+//                             </a>
 
-                            <div className="dropdown-menu" aria-labelledby="dropdownMenuLink">
-                                {this.props.product.images.map((option) => {
-                                    return  <a className="dropdown-item" 
-                                               href="#"
-                                               key={option.color}
-                                               onClick={() => this.handleColorOptionChange(option.color)}>
-                                               {option.color}
-                                            </a>
-                                })}
-                            </div>
-                        </div>
+//                             <div className="dropdown-menu" aria-labelledby="dropdownMenuLink">
+//                                 {this.props.product.images.map((option) => {
+//                                     return  <a className="dropdown-item" 
+//                                                href="#"
+//                                                key={option.color}
+//                                                onClick={() => this.handleColorOptionChange(option.color)}>
+//                                                {option.color}
+//                                             </a>
+//                                 })}
+//                             </div>
+//                         </div>
 
-                        <button 
-                            className="btn"
-                            onClick={() => this.addToCart()}>
-                            {this.state.cartText}
-                        </button>
-                        <span>
-                                {
-                                    this.state.inCart
-                                        ? "Item added to cart!"
-                                        : ""
-                                }
-                        </span>
-                    </div>     
+//                         <button 
+//                             className="btn"
+//                             onClick={() => this.addToCart()}>
+//                             {this.state.cartText}
+//                         </button>
+//                         <span>
+//                                 {
+//                                     this.state.inCart
+//                                         ? "Item added to cart!"
+//                                         : ""
+//                                 }
+//                         </span>
+//                     </div>     
 
-                    <div className="col-12 col-md-6">
-                        <ProductImage image={this.state.src} name={name} />                        
-                    </div>
+//                     <div className="col-12 col-md-6">
+//                         <ProductImage image={this.state.src} name={name} />                        
+//                     </div>
 
 
 
-                </div>
-            </div>
-        );
-    }
-}
+//                 </div>
+//             </div>
+//         );
+//     }
+// }
+
+
+const Product =({
+    product,
+    setProduct  
+  }) => (
+    <div
+        onClick={() => setProduct({
+           ...product,
+           name: "foo" 
+        })}
+    >
+        {JSON.stringify(product)}
+    </div>
+  )
 
 export default Product;
